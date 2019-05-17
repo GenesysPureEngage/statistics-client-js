88ddf1e0560940221a83650ea1005208bfaf7a62
maksimlysenko <maxim.lysenko@genesys.com>
2019-05-17 06:17:49 +0000

    Release: 9.0.19

--- src/notifications.js
+++ src/notifications.js
@@ -1,24 +1,24 @@
+require('./cometd-nodejs-client').adapt();
 const url = require('url');
 const EventEmitter = require('events');
 const logger = require('winston');
 
 const CookieJar = require('cookiejar').CookieJar;
 const cometDLib = require('cometd');
-require('./cometd-nodejs-client').adapt();
 
 class Notifications extends EventEmitter {
     constructor(apiKey, endpointUrl) {
         super();
-        
+
         this.apiKey = apiKey;
         this.endpointUrl = endpointUrl;
         this.cometd = new cometDLib.CometD();
     }
-    
+
     initialize(token, channels = [], cookieJar = new CookieJar(), cometdConfiguration = {}) {
         const cometd = this.cometd;
         logger.debug(`Initializing (token: ${token}, channels: ${channels})`);
-        
+
         return new Promise((resolve, reject) => {
             const hostname = url.parse(this.endpointUrl).hostname;
             const transport = cometd.findTransport('long-polling');
@@ -40,29 +40,29 @@ class Notifications extends EventEmitter {
                 cometd.configure(cometdConfiguration);
             }
             cometd.handshake(reply => {
-                if(reply.successful) {
+                if (reply.successful) {
                     logger.debug('Handshake successful');
-                    
+
                     channels.forEach(channel => {
                         cometd.subscribe(channel, msg => {
                             logger.debug(msg);
                             const data = msg.data;
                             this.emit(channel, data);
                         }, result => {
-                            if(result.successful) {
+                            if (result.successful) {
                                 logger.debug(`Successfully subscribed: ${channel}`);
                             } else {
                                 reject(`Cannot subscribe: ${channel}`);
                             }
                         });
                     });
-                    
+
                     resolve();
                 }
-          });
-      });
+            });
+        });
     }
-    
+
     finalize() {
         this.cometd.disconnect();
     }
